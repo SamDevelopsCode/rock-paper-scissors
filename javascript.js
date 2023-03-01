86eb1b2f31edc5a6abc1923ac10ad489e922cfb6
samdevelopscode <fousek.sam@gmail.com>
2023-03-01 17:44:46 -0600

    Added all functions necessary to complete game. Tested.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,12 @@
+let playerScore = 0;
+let computerScore = 0;
+
+function showGameIntro() {
+    console.log("Welcome to another exciting game of ROCK, PAPER, SCISSORS! Follow the onscreen prompt to pick your weapon and good luck! First to 3 points")
+}
+
 function getComputerChoice() {
-    
+
     let computerChoices = ["rock", "paper", "scissors"];    
     let randomNumber = Math.random() * 3;
     let computerChoice;
@@ -21,8 +28,101 @@ function getComputerChoice() {
 }
 
 function getPlayerChoice() {
+
     let playerChoiceRaw = prompt("Choose your weapon: rock, paper, or scissors. Be sure to type carefully and then hit enter!");
     let playerChoice = playerChoiceRaw.toLowerCase();
-
-    console.log(playerChoice);
+    return playerChoice;
 }
+
+function playOneRound() {
+
+    chooseRoundWinner(getPlayerChoice(), getComputerChoice());    
+}
+
+function chooseRoundWinner(playerChoice, computerChoice){
+
+    if (playerChoice == "rock" && computerChoice == "rock")
+    {
+        console.log("DRAW!");
+        showScores();
+        return;
+    }
+    else if (playerChoice == "rock" && computerChoice == "paper")
+    {
+        computerScore++;
+        showScores();
+        return;
+    }
+    else if (playerChoice == "rock" && computerChoice == "scissors")
+    {
+        playerScore++;
+        showScores();
+        return;
+    }
+    else if (playerChoice == "paper" && computerChoice == "paper")
+    {
+        console.log("DRAW!");
+        showScores();
+        return;
+    }
+    else if (playerChoice == "paper" && computerChoice == "scissors")
+    {
+        computerScore++;
+        showScores();
+        return;
+    }
+    else if (playerChoice == "paper" && computerChoice == "rock")
+    {
+        playerScore++;
+        showScores();
+        return;
+    }
+    else if (playerChoice == "scissors" && computerChoice == "scissors")
+    {
+        console.log("DRAW!");
+        showScores();
+        return;
+    }
+    else if (playerChoice == "scissors" && computerChoice == "rock")
+    {
+        computerScore++;
+        showScores();
+        return;
+    }
+    else if (playerChoice == "scissors" && computerChoice == "paper")
+    {
+        playerScore++;
+        showScores();
+        return;
+    }   
+    else
+    {
+        console.log("Check your spelling! You chose an invalid weapon. Try again.");
+        return;
+    }
+}
+
+function showScores(){
+    console.log("Player: " + playerScore);
+    console.log("Computer: " + computerScore);
+}
+
+function playGame() {
+
+    showGameIntro();
+    while (playerScore < 3 && computerScore < 3)
+    {
+        playOneRound();
+        if (playerScore == 3)
+        {
+            console.log("WINNER => Player");
+        }
+        else if (computerScore == 3)
+        {
+            console.log("WINNER => Computer");
+        }
+    }
+}
+
+playGame();
+
